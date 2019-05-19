1c188a456aa29b02e983326f12ff0a03009c2a52
dixmw34 <48245412+dixmw34@users.noreply.github.com>
2019-05-20 00:21:13 +0900

    Add files via upload

---
+++ 유니티 미니점령전 소스코드/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shoot : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Destroy(gameObject, 2f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)//부딪히면 실행됨. 파라미터는 부딪힌 대상이 들어옴
+    {
+        Destroy(gameObject);
+
+    }
+}
